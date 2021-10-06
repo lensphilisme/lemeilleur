1369e9d89d913b213b8f67408dd3fb93073fd1fa
Lens-Chaédarson Philismé <73264885+lensphilisme@users.noreply.github.com>
2021-10-06 19:50:24 -0400

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -81,7 +81,7 @@ export default function About() {
           <div className="top-section">
             <div className="left">
               <p className="about__subheading">
-                Hi, I am <span>Ayan Khan</span>
+                Salut,je suis <span>Lens Philismé</span>
               </p>
               <h2 className="about__heading">A freelance Web developer</h2>
               <div className="about__info">
